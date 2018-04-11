43caa11b50e9ccd456ea336390cbde40e0ccd513
desaiankitb <desaiankitb@gmail.com>
2018-04-11 20:25:03 +0530

    fix: path changes for train test files

--- 04/model_logging final.py
+++ 04/model_logging final.py
@@ -14,7 +14,7 @@ X_training = training_data_df.drop('total_earnings', axis=1).values
 Y_training = training_data_df[['total_earnings']].values
 
 # Load testing data set from CSV file
-test_data_df = pd.read_csv("/home/desaiankitb/Code/TensorFlow_beginners/04/sales_data_test.csv", dtype=float)
+test_data_df = pd.read_csv("/home/desaiankitb/Code/TensorFlow_beginners/04//home/desaiankitb/Code/TensorFlow_beginners/04sales_data_test.csv", dtype=float)
 
 # Pull out columns for X (data to train with) and Y (value to predict)
 X_testing = test_data_df.drop('total_earnings', axis=1).values
